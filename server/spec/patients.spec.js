fd267f00a18f1b66260771d767b2b95f1f6fc756
saipranithswargam <saipranithswargam@gmail.com>
2024-04-30 16:42:01 +0530

    minute changes

--- server/spec/patients.spec.js
+++ server/spec/patients.spec.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const app = require('../app');
-const Hospitals = require('../models/hospitals')
 describe('POST /patients/login', () => {
     it('should login a patient with valid credentials', async () => {
         const response = await request(app)
@@ -16,31 +15,3 @@ describe('POST /patients/login', () => {
         expect(response.status).toBe(401);
     });
 });
-
-// describe('POST /patients/register', () => {
-//     it('should register a new patient with valid data', async () => {
-//         //change the mail id to test this feature 
-//         const newPatientData = {
-//             name: 'John Doe',
-//             email: 'johndoe@example.com',
-//             password: 'password123',
-//             mobileNumber: '1234567890',
-//             height: 180,
-//             weight: 75,
-//             bloodGroup: 'O+',
-//             state: 'California',
-//             city: 'Los Angeles',
-//             pincode: '90001',
-//             age: 30,
-//             gender: 'male',
-//             maritalStatus: 'yes',
-//             allergies: "'Pollen', 'Peanuts'"
-//         };
-
-//         const response = await request(app)
-//             .post('/patients/register')
-//             .send(newPatientData);
-//         expect(response.status).toBe(200);
-//         expect(response.body.message).toEqual('success');
-//     });
-// });
